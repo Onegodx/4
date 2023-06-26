bdbd2a1a4515d13690fbf03a1bbbe60d6f7dd41f
Onegodx <113604166+Onegodx@users.noreply.github.com>
2023-06-26 14:43:36 +0200

    Update Class1.cs

--- ConsoleApp14/Class1.cs
+++ ConsoleApp14/Class1.cs
@@ -1,58 +1,33 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace ConsoleApp14
+public class NOTE : ICloneable, IComparable<NOTE>
+//Миньков владимир 22исп21 15 вариант
 {
-     class zodiak
+    public string LastName { get; set; }
+    public string FirstName { get; set; }
+    public string PhoneNumber { get; set; }
+
+    public object Clone()
     {
-        public class zodiak_1 : ICloneable, IComparable<zodiak_1>
+        return new NOTE
         {
-            public string FirstName { get; set; }
-            public string LastName { get; set; }
-            public string Zodiac { get; set; }
-            public int ID { get; set; }
-            public int[] BirthDate { get; set; }
+            LastName = this.LastName,
+            FirstName = this.FirstName,
+            PhoneNumber = this.PhoneNumber
+        };
+    }
 
-            // Конструктор класса
-            public zodiak_1(string firstName, string lastName, string zodiac, int id, int[] birthDate)
-            {
-                FirstName = firstName;
-                LastName = lastName;
-                Zodiac = zodiac;
-                ID = id;
-                BirthDate = birthDate;
-            }
+    public int CompareTo(NOTE other)
+    {
+        return this.PhoneNumber.Substring(0, 3).CompareTo(other.PhoneNumber.Substring(0, 3));
+    }
 
-            // Реализация интерфейса ICloneable
-            public object Clone()
-            {
-                return new zodiak_1(FirstName, LastName, Zodiac, ID, BirthDate);
-            }
-
-            // Реализация интерфейса IComparable
-            public int CompareTo(zodiak_1 other)
-            {
-                DateTime thisBirthDate = new DateTime(BirthDate[2], BirthDate[1], BirthDate[0]);
-                DateTime otherBirthDate = new DateTime(other.BirthDate[2], other.BirthDate[1], other.BirthDate[0]);
-                return thisBirthDate.CompareTo(otherBirthDate);
-            }
-
-            // Класс для сортировки по Фамилии, Имени
-            public class NameComparer : IComparer<zodiak_1>
-            {
-                public int Compare(zodiak_1 x, zodiak_1 y)
-                {
-                    int comp = x.LastName.CompareTo(y.LastName);
-                    if (comp == 0)
-                    {
-                        return x.FirstName.CompareTo(y.FirstName);
-                    }
-                    return comp;
-                }
-            }
-        }
+    public override string ToString()
+    {
+        return $"{LastName} {FirstName} - {PhoneNumber}";
     }
 }
