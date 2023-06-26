2f2c494adc82d6f7eece532de4e9d42620c6fb75
Onegodx <113604166+Onegodx@users.noreply.github.com>
2023-06-26 14:44:17 +0200

    Update Program.cs

--- ConsoleApp14/Program.cs
+++ ConsoleApp14/Program.cs
@@ -1,85 +1,166 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+//Миньков Владимир вариант 15 
 
-
-using ConsoleApp14;
-
-class zodiak
+public static class NoteHelper
 {
-    public zodiak(string? firstName, string? lastName, string? zodiac, int id, int[] birthDate)
+    public static NOTE[] InputNotes(int count)
     {
-        FirstName = firstName;
-        LastName = lastName;
-        Zodiac = zodiac;
-        Id = id;
-        BirthDate = birthDate;
+        NOTE[] notes = new NOTE[count];
+        for (int i = 0; i < count; i++)
+        {
+            notes[i] = new NOTE();
+            Console.WriteLine($"Введите данные записи {i + 1}:");
+            Console.Write("Введите фамилию: ");
+            notes[i].LastName = Console.ReadLine();
+            Console.Write("Введите имя: ");
+            notes[i].FirstName = Console.ReadLine();
+            Console.Write("Введите номер телефона: ");
+            notes[i].PhoneNumber = Console.ReadLine();
+        }
+        Array.Sort(notes);
+        return notes;
     }
 
-    public string? FirstName { get; }
-    public string? LastName { get; }
-    public string? Zodiac { get; }
-    public int Id { get; }
-    public int ID { get; private set; }
-    public int[] BirthDate { get; }
-
-    static void Main(string[] args)
+    public static void OutputNotes(NOTE[] notes)
     {
-        zodiak[] znaks = new zodiak[8];
-
-        // Ввод данных с клавиатуры
-        for (int i = 0; i < 8; i++)
+        Console.WriteLine("Список записей:");
+        foreach (var note in notes)
         {
-            Console.WriteLine("Введите информацию о человеке номер {0}", i + 1);
-            Console.Write("Фамилия: ");
-            string lastName = Console.ReadLine();
-            Console.Write("Имя: ");
-            string firstName = Console.ReadLine();
-            Console.Write("Знак Зодиака: ");
-            string zodiac = Console.ReadLine();
-            Console.Write("Идентификационный номер: ");
-            int id = int.Parse(Console.ReadLine());
-            Console.Write("Дата рождения (дд.мм.гггг): ");
-            string[] birthDateStr = Console.ReadLine().Split('.');
-            int[] birthDate = new int[3];
-            for (int j = 0; j < 3; j++)
-            {
-                birthDate[j] = int.Parse(birthDateStr[j]);
-            }
-            znaks[i] = new zodiak(firstName, lastName, zodiac, id, birthDate);
+            Console.WriteLine(note);
         }
+    }
 
-        // Сортировка по дате рождения
-        Array.Sort(znaks);
+    public static NOTE FindNoteByLastName(NOTE[] notes, string lastName)
+    {
+        foreach (var note in notes)
+        {
+            if (note.LastName.Equals(lastName, StringComparison.OrdinalIgnoreCase))
+            {
+                return (NOTE)note.Clone();
+            }
+        }
+        return null;
+    }
 
-        // Вывод информации о человеке по фамилии
+    public static void SearchNoteByLastName(NOTE[] notes)
+    {
         Console.Write("Введите фамилию для поиска: ");
-        string searchLastName = Console.ReadLine();
-        bool found = false;
-        foreach (zodiak z in znaks)
+        string lastName = Console.ReadLine();
+        NOTE note = FindNoteByLastName(notes, lastName);
+        if (note != null)
         {
-            if (z.LastName == searchLastName)
+            Console.WriteLine($"Найдена запись: {note}");
+        }
+        else
+        {
+            Console.WriteLine($"Запись с фамилией {lastName} не найдена.");
+        }
+    }
+}
+public class ZNAK<T> : ICloneable, IComparable<ZNAK<T>>
+{
+    public string LastName { get; set; }
+    public string FirstName { get; set; }
+    public string Zodiac { get; set; }
+    public T IdentificationNumber { get; set; }
+    public int[] Birthday { get; set; }
+
+    public object Clone()
+    {
+        return new ZNAK<T>
+        {
+            LastName = this.LastName,
+            FirstName = this.FirstName,
+            Zodiac = this.Zodiac,
+            IdentificationNumber = this.IdentificationNumber,
+            Birthday = (int[])this.Birthday.Clone()
+        };
+    }
+
+    public int CompareTo(ZNAK<T> other)
+    {
+        int result = 0;
+        for (int i = 0; i < Birthday.Length; i++)
+        {
+            result = Birthday[i].CompareTo(other.Birthday[i]);
+            if (result != 0) break;
+        }
+        return result;
+    }
+
+    public override string ToString()
+    {
+        return $"{LastName} {FirstName} - {string.Join(".", Birthday)} ({Zodiac})";
+    }
+}
+
+public static class ZnakHelper
+{
+    public static ZNAK<T>[] InputZnaks<T>(int count)
+    {
+        ZNAK<T>[] znaks = new ZNAK<T>[count];
+        for (int i = 0; i < count; i++)
+        {
+            znaks[i] = new ZNAK<T>();
+            Console.WriteLine($"Введите данные записи {i + 1}:");
+            Console.Write("Введите фамилию: ");
+            znaks[i].LastName = Console.ReadLine();
+            Console.Write("Введите имя: ");
+            znaks[i].FirstName = Console.ReadLine();
+            Console.Write("Введите знак зодиака: ");
+            znaks[i].Zodiac = Console.ReadLine();
+            Console.Write("Введите идентификационный номер: ");
+            znaks[i].IdentificationNumber = (T)(object)Console.ReadLine();
+            znaks[i].Birthday = new int[3];
+            Console.WriteLine("Введите дату рождения:");
+            Console.Write("День: ");
+            znaks[i].Birthday[0] = int.Parse(Console.ReadLine());
+            Console.Write("Месяц: ");
+            znaks[i].Birthday[1] = int.Parse(Console.ReadLine());
+            Console.Write("Год: ");
+            znaks[i].Birthday[2] = int.Parse(Console.ReadLine());
+        }
+        Array.Sort(znaks);
+        return znaks;
+    }
+
+    public static void OutputZnaks<T>(ZNAK<T>[] znaks)
+    {
+        Console.WriteLine("Список записей:");
+        foreach (var znak in znaks)
+        {
+            Console.WriteLine(znak);
+        }
+    }
+
+    public static ZNAK<T> FindZnakByLastName<T>(ZNAK<T>[] znaks, string lastName)
+    {
+        foreach (var znak in znaks)
+        {
+            if (znak.LastName.Equals(lastName, StringComparison.OrdinalIgnoreCase))
             {
-                Console.WriteLine("Информация о человеке:");
-                Console.WriteLine("Фамилия: {0}", z.LastName);
-                Console.WriteLine("Имя: {0}", z.FirstName);
-                Console.WriteLine("Знак Зодиака: {0}", z.Zodiac);
-                Console.WriteLine("Идентификационный номер: {0}", z.ID);
-                Console.WriteLine("Дата рождения: {0:d}", new DateTime(z.BirthDate[2], z.BirthDate[1], z.BirthDate[0]));
-                found = true;
-                break;
+                return (ZNAK<T>)znak.Clone();
             }
         }
-        if (!found)
+        return null;
+    }
+
+    public static void SearchZnakByLastName<T>(ZNAK<T>[] znaks)
+    {
+        Console.Write("Введите фамилию для поиска: ");
+        string lastName = Console.ReadLine();
+        ZNAK<T> znak = FindZnakByLastName(znaks, lastName);
+        if (znak != null)
         {
-            Console.WriteLine("Человек с такой фамилией не найден");
+            Console.WriteLine($"Найдена запись: {znak}");
         }
-
-        // Сортировка по Фамилии, Имени
-        Array.Sort(znaks, new zodiak.NameComparer());
-
-        Console.ReadLine();
+        else
+        {
+            Console.WriteLine($"Запись с фамилией {lastName} не найдена.");
+        }
     }
 }
